74fc1e007c8d51b0566abc68f8ac75ea5fd9633e
Javon Browne <javonbrowne@gmail.com>
2025-11-16 23:19:12 -0400

    Finding the sum, product & average of two numbers

---
+++ task2.py
@@ -0,0 +1,18 @@
+# Adding two number and printng the result
+def add_numbers(a, b):
+    return a + b
+
+result  = add_numbers(5, 7)
+print("The sum is:", result)
+
+# The producton of the two numbers
+def multiply_numbers(a, b): 
+    return a * b
+result = multiply_numbers(5, 7)
+print("The product is:", result)
+
+# The average of the two numbers
+def average_numbers(a, b):
+    return (a + b) / 2
+result = average_numbers(5, 7)
+print("The average is:", result)
