3937bd8a42dc8ea9d86c3cafff32349c20cc6aa6
Javon Browne <javonbrowne@gmail.com>
2025-11-17 18:13:22 -0400

    Modified output with an f-string

--- task1.py
+++ task1.py
@@ -4,4 +4,5 @@ def hello_world():
 def my_name():
     return "Javon Browne"
 if __name__ == "__main__":
-    print(hello_world(),"Ny name is", my_name())
+    # print(hello_world(),"Ny name is", my_name())
+    print(f"{hello_world()} My name is {my_name()}")
