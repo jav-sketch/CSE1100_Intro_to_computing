55d16168791510dbb50753f9a29fbbb863bb2a91
Javon Browne <javonbrowne@gmail.com>
2025-11-16 23:11:13 -0400

    Modified code to print my name using functions

--- task1.py
+++ task1.py
@@ -1,4 +1,7 @@
 def hello_world():
     return "Hello, World!"
+
+def my_name():
+    return "Javon Browne"
 if __name__ == "__main__":
-    print(hello_world())
+    print(hello_world(),"Ny name is", my_name())
